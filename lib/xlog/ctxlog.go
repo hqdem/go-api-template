6595e8d1cf08c552f87c69b8866a7f7add3558d9
hqdem <107410043+hqdem@users.noreply.github.com>
2024-08-29 19:04:37 +0300

    GOAPITEMPL-8: add context log fields (#8)

---
+++ lib/xlog/ctxlog.go
@@ -0,0 +1,34 @@
+package xlog
+
+import (
+	"context"
+	"go.uber.org/zap"
+)
+
+const ctxFieldsKey = "ctx_fields"
+
+func WithFields(ctx context.Context, fields ...zap.Field) context.Context {
+	if len(fields) == 0 {
+		return ctx
+	}
+	return context.WithValue(ctx, ctxFieldsKey, mergeFields(GetContextFields(ctx), fields))
+}
+
+func GetContextFields(ctx context.Context) []zap.Field {
+	fields, _ := ctx.Value(ctxFieldsKey).([]zap.Field)
+	return fields
+}
+
+func mergeFields(oldFields, newFields []zap.Field) []zap.Field {
+	if len(oldFields) == 0 {
+		return newFields
+	}
+	if len(newFields) == 0 {
+		return oldFields
+	}
+
+	fields := make([]zap.Field, len(oldFields)+len(newFields))
+	n := copy(fields, oldFields)
+	_ = copy(fields[n:], newFields)
+	return fields
+}
