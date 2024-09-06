8fb56f656937a279d56b1f8125b65e2be427891c
hqdem <ilyadem4@gmail.com>
2024-09-07 00:28:58 +0300

    GOAPITEMPL-21: test job

--- cmd/runserver/main.go
+++ cmd/runserver/main.go
@@ -2,10 +2,13 @@ package runserver
 
 import (
 	"github.com/hqdem/go-api-template/pkg/commands/runserver"
+	"github.com/labstack/echo/v4"
 	"github.com/spf13/cobra"
 )
 
 func CreateCommand(cfgPath *string) *cobra.Command {
+
+	_ = echo.New()
 	return &cobra.Command{
 		Use:   "run --config /path/to/cfg",
 		Short: "run api server",
